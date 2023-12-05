6768f0ca2acc6585d6c77ea522fb18beba64c008
Зайнагтдинова Галия Нафисовна <GZaynagtdinova@beeline.ru>
2023-12-05 09:43:58 +0300

    SFDM-1176 : создание репозитория для технорадара

---
+++ src/main/java/ru/beeline/techradar/TechradarBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.beeline.techradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechradarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechradarBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/beeline/techradar/TechradarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.beeline.techradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechradarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
