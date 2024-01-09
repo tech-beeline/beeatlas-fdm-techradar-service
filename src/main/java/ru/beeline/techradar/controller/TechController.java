d6331ddf915f27a9bf88a8cd41a3859b47a061a3
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2024-01-09 17:55:49 +0300

    SFDM-1166 fix default scheme

--- src/main/java/ru/beeline/techradar/controller/TechController.java
+++ src/main/java/ru/beeline/techradar/controller/TechController.java
@@ -1,7 +1,5 @@
 package ru.beeline.techradar.controller;
 
-import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
