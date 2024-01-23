9f4986434a5c29804451be47d5208efc7bf1fecd
Зайнагтдинова Галия Нафисовна <GZaynagtdinova@beeline.ru>
2024-01-23 08:58:47 +0300

    SFDM-1167: add API method getAllCategories

---
+++ src/main/java/ru/beeline/techradar/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package ru.beeline.techradar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.beeline.techradar.domain.Category;
+import ru.beeline.techradar.service.CategoryService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAll();
+    }
+}
