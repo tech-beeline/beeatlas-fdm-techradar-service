d395ce401c8f939c2be7d724893b72b281681c80
Зайнагтдинова Галия Нафисовна <GZaynagtdinova@beeline.ru>
2024-01-25 12:17:43 +0300

    SFDM-1168: add API method getTechByCategories

---
+++ src/main/java/ru/beeline/techradar/repository/TechCategoryRepository.java
@@ -0,0 +1,9 @@
+package ru.beeline.techradar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.beeline.techradar.domain.TechCategory;
+import java.util.List;
+
+public interface TechCategoryRepository extends JpaRepository<TechCategory, Long> {
+    List<TechCategory> findByCategory_IdIn(List<Integer> ids);
+}
